aa8f8ea9f640fb397652f94e034c013242b5a869
Anand Pasunoori <anand853@gmail.com>
2016-05-13 17:16:28 -0400

    added config props
    Change-Id: I38809e5abca1618f36754ce870ca21949341b202
    Signed-off-by: Anand Pasunoori <anand853@gmail.com>

---
+++ WebAutomation/src/main/java/com/google/test/google/App.java
@@ -0,0 +1,14 @@
+package com.google.test.google;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		Runner run = new Runner();
+		run.lauchDriver("chrome");
+		
+	}
+}
